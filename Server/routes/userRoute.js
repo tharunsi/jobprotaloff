62a7594e1345b52140e894d279a19670bfcd417f
tharunsi Name <tharunsitharunsi@gmail.com>
2024-09-13 19:03:55 +0530

    and

--- Server/routes/userRoute.js
+++ Server/routes/userRoute.js
@@ -33,9 +33,9 @@ route.post("/logindetail", async (req, res) => {
         const name = data.name;
         const token = jwt.sign({name},  process.env.JWT_SECRET,{expiresIn: '1d'});
         res.cookie('token', token, {
-            httpOnly: true, 
-            secure: true, 
-            sameSite: 'None', // Required for cross-site cookies in HTTPS
+            // httpOnly: true, 
+            // secure: true, 
+            // sameSite: 'None', // Required for cross-site cookies in HTTPS
             maxAge: 24 * 60 * 60 * 1000 // 1 day expiration
         });
         
