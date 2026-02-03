97904bd8d637216bb60688c3168a757586ad251d
tharunsi Name <tharunsitharunsi@gmail.com>
2026-02-03 23:37:32 +0530

    main

--- client/src/Pages/Profile/profile.jsx
+++ client/src/Pages/Profile/profile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import "font-awesome/css/font-awesome.min.css";
 import './profile.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
