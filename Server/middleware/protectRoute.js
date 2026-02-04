4cf9f8dabca24d45ec4529c207ff44bcc9fe8b4b
tharunsi Name <tharunsitharunsi@gmail.com>
2026-02-04 15:13:28 +0530

    1

--- Server/middleware/protectRoute.js
+++ Server/middleware/protectRoute.js
@@ -3,7 +3,7 @@ import { user } from '../models/User.js';
 
 const protectRoute = async (req, res , next) => {
     try{
-       const token = req.cookies.token;
+       const token = req.cookies.jwt;
 
       
         if(!token) {
